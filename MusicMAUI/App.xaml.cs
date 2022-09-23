9fa5711264c05065c23c0ad3629d72f0c0f1a041
Ilya <Ilya@WIN-HKO9MDQ47H1>
2022-09-23 20:37:47 +0300

    5

---
+++ MusicMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MusicMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
