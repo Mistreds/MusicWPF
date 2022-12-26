50e221e58361fee07f079ac61a1fe1fe007e1a38
Mistred <spiridonov24@gmail.com>
2022-12-26 19:41:13 +0300

    q

--- MusicLibMAUI/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicLibMAUI
-{
-    // All the code in this file is included in all platforms.
-    public class Class1
-    {
-
-    }
-}
--- MusicWidget/App.xaml.cs
+++ MusicWidget/App.xaml.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace MusicWidget
 {
@@ -16,31 +13,5 @@ namespace MusicWidget
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-
-            // ConsoleHelper.AllocConsole();
-            this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
-        }
-        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-
-            Process.Start("MusicWidget.exe");
-            Process.GetCurrentProcess().Kill();
-        }
-        public class ConsoleHelper
-        {
-            /// <summary>
-            /// Allocates a new console for current process.
-            /// </summary>
-            [DllImport("kernel32.dll")]
-            public static extern Boolean AllocConsole();
-
-            /// <summary>
-            /// Frees the console.
-            /// </summary>
-            [DllImport("kernel32.dll")]
-            public static extern Boolean FreeConsole();
-        }
     }
 }
